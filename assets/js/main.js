63a34b3f56e1fc38a16872e85d9f2ea96c30be7e
rupin_desai <rupin360@gmail.com>
2025-02-06 15:35:36 +0530

    Refactor countdown display and enhance carousel item initialization; update Participate Now link for better accessibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -636,7 +636,7 @@ function startCountdown(targetDate, daysId, hoursId, minutesId, secondsId) {
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       document.getElementById(daysId).innerHTML = days + "<br/><span>DAYS</span>";
-      document.getElementById(hoursId).innerHTML = hours + " <span>Hours</span>";
+      document.getElementById(hoursId).innerHTML = hours + " <br/><span>Hours</span>";
       document.getElementById(minutesId).innerHTML = minutes + " <span>Minutes</span>";
       document.getElementById(secondsId).innerHTML = seconds + " <span>Seconds</span>";
 
